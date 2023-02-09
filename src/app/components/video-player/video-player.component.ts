e312228a6ffd2264cd4cdfb7b8cbdee7685230fe
Miriam Marin <mir.marin.df@gmail.com>
2023-02-08 22:30:40 -0600

    configuration tensor

--- src/app/components/video-player/video-player.component.ts
+++ src/app/components/video-player/video-player.component.ts
@@ -1,5 +1,13 @@
-import { AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
-
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+// import * as canvas from 'canvas';
+import * as faceapi from 'face-api.js'
+/**
+ * Component to use de media device cam
+ *
+ * @export
+ * @class VideoPlayerComponent
+ * @implements {OnInit}
+ */
 @Component({
   selector: 'app-video-player',
   templateUrl: './video-player.component.html',
@@ -12,18 +20,31 @@ export class VideoPlayerComponent implements OnInit {
    /**
    * Request user access to cam otherwise send error
    *
-   * @memberof CardComponent
+   * @memberof VideoPlayerComponent
    */
   async accessCamera(){
     try{
-      let video = await navigator.mediaDevices.getUserMedia({video: true})
+      let video = await navigator.mediaDevices.getUserMedia({video: true});
       let streams = this.videoElement.nativeElement;
+
+      await faceapi.nets.tinyFaceDetector.loadFromUri('./assets/models/');
+      await faceapi.nets.faceLandmark68Net.loadFromUri('./assets/models/');
+      await faceapi.nets.faceExpressionNet.loadFromUri('./assets/models/');
+
       streams.srcObject = video;
-      streams.play()
+      streams.play();
     } catch(err) {
       // ToDo: Change alert to a modal
-      alert('Necesitamos permiso para accesar a tu cámara')
+      alert('Necesitamos permiso para accesar a tu cámara');
     }
+  }
+
+
+  playCamera(){
+    this.videoElement.nativeElement.addEventListener('play', ()=>{
+      const canvas = faceapi.createCanvasFromMedia(this.videoElement.nativeElement);
+      document.body.append(canvas);
+    })
   }
 
 
